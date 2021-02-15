c1f6eae00850607eca792b055bb389dec0de2fd8
CindoddCindy <cindodcindy@gmail.com>
2021-02-15 21:45:28 +0700

    cuma tmbah folder

---
+++ reqmapping/src/main/java/com/examplereqmapping/reqmapping/ReqmappingApplication.java
@@ -0,0 +1,13 @@
+package com.examplereqmapping.reqmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqmappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReqmappingApplication.class, args);
+	}
+
+}
---
+++ reqmapping/src/test/java/com/examplereqmapping/reqmapping/ReqmappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplereqmapping.reqmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReqmappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
